dffe200a1538da2d715166b5dafaa88632ef5c66
ClaudioS86 <secciclaudio86@gmail.com>
2025-06-17 22:37:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, send_file
+from get_aspects.py import run_aspect_calc  # Importa la funzione
 from swisseph_utils import calculate_birth_chart
 import os  # <-- IMPORTANTE per leggere la porta da Railway
 
@@ -27,6 +28,31 @@ def birth_chart():
 
     except Exception as e:
         traceback.print_exc()  # <-- aggiungi questa riga
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/aspects', methods=['POST'])   # ⬅️ deve essere allineato a quello sopra
+def aspects():
+    try:
+        data = request.get_json()
+        
+        birth_date = data["birth_date"]
+        birth_time = data["birth_time"]
+        lat = data["lat"]
+        lon = data["lon"]
+        timezone = float(data.get("timezone", 0))
+
+        result = run_aspect_calc(
+            birth_date,
+            birth_time,
+            lat,
+            lon,
+            timezone
+        )
+
+        return jsonify(result), 200
+
+    except Exception as e:
+        traceback.print_exc()
         return jsonify({"error": str(e)}), 500
          
 # ✅ QUESTA PARTE È FONDAMENTALE PER RAILWAY
